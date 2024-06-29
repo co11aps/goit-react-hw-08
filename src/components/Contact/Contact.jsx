16c64d918e34a89e4c0ffd924a6a32bc9e8faf6b
co11aps <143988142+co11aps@users.noreply.github.com>
2024-06-30 00:24:59 +0800

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,6 @@ import css from "./Contact.module.css";
 import { BsFillPersonFill } from "react-icons/bs";
 import { BsFillTelephoneFill } from "react-icons/bs";
 
-import { AiOutlineClose } from "react-icons/ai";
 // import { LuPencil } from "react-icons/lu";
 import { useDispatch } from "react-redux";
 import { deleteContact, editContact } from "../../redux/contacts/operations";
@@ -29,15 +28,16 @@ const Contact = ({ contact }) => {
     <div className={css.item}>
       <div>
         <p className={css.iconName}>
-          <BsFillPersonFill size={20} /> {name}
+          <BsFillPersonFill size={14} className={css.icon} /> {name}
         </p>
-        <p>
-          <BsFillTelephoneFill /> {number}
-        </p>
+        <a href={`tel:${number.split("-").join("")}`} className={css.tel}>
+          <BsFillTelephoneFill size={14} className={css.icon} />
+          {number}
+        </a>
       </div>
       <div className={css.buttonDiv}>
         <button className={css.btn} onClick={handleDeleteContact}>
-          <AiOutlineClose />
+          Delete
         </button>
         {/* <button className={css.btn} onClick={handleEditContact}>
           <LuPencil size={16} />
@@ -48,18 +48,3 @@ const Contact = ({ contact }) => {
 };
 
 export default Contact;
-
-// export const Task = ({ id, text }) => {
-//   const dispatch = useDispatch();
-
-//   const handleDelete = () => dispatch(deleteTask(id));
-
-//   return (
-//     <div className={css.wrapper}>
-//       <p className={css.text}>{text}</p>
-//       <button type="button" className={css.button} onClick={handleDelete}>
-//         Delete
-//       </button>
-//     </div>
-//   );
-// };
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,24 +1,35 @@
 import { useDispatch } from "react-redux";
 import { changeFilter } from "../../redux/filters/slice";
 import css from "./SearchBox.module.css";
+import { useState } from "react";
 
 const SearchBox = () => {
+  const [inputValue, setInputValue] = useState("");
   const dispatch = useDispatch();
 
   const handleInputChange = (e) => {
     const newValue = e.target.value;
+    setInputValue(newValue);
     dispatch(changeFilter(newValue));
+  };
+
+  const resetFilter = () => {
+    setInputValue("");
+    dispatch(changeFilter(""));
   };
 
   return (
     <div className={css.contact}>
-      {/* <label className={css.searchLabel}>Find contacts by name</label> */}
       <input
         className={css.input}
         type="text"
+        value={inputValue}
         onChange={handleInputChange}
         placeholder="Find contacts by name/number"
       />
+      <button className={css.btn} onClick={resetFilter}>
+        Reset
+      </button>
     </div>
   );
 };
